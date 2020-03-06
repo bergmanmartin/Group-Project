839fde76f014f0fdbb75c2caa220f46256a72625
bergmanmartin <59979866+bergmanmartin@users.noreply.github.com>
2020-03-06 14:53:14 +0100

    Add files via upload

---
+++ StartClass.java
@@ -0,0 +1,10 @@
+package GameClasses;
+
+public class StartClass {
+
+    public static void main(String[] args) {
+        Numbers gameObj = new Numbers();
+        gameObj.startGame();
+
+    }
+}
